14662cb8e71a282eccb0ca4a299c6e091305f454
Bas van der Hoek <bas@hoek.com>
2022-07-27 09:11:18 +0200

    add tailrec annotations

--- src/test/scala/ChapterTwoSpec.scala
+++ src/test/scala/ChapterTwoSpec.scala
@@ -1,8 +1,10 @@
 import org.scalatest.flatspec.AnyFlatSpec
+import scala.annotation.tailrec
 
 class ChapterTwoSpec extends AnyFlatSpec {
 
   def fib(n: Int): Int = {
+    @tailrec
     def go(n: Int, a: Int, b: Int): Int =
       if (n <= 0) a else go(n - 1, b, a + b)
 
@@ -11,6 +13,32 @@ class ChapterTwoSpec extends AnyFlatSpec {
 
   "The eight Fibonacci number" should "be 21" in {
     assert(fib(8) == 21)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @tailrec
+    def go(n: Int): Boolean =
+      if n < as.length - 1 then
+        if ordered(as(n), as(n + 1)) then go(n + 1) else false
+      else true
+
+    go(0)
+  }
+
+  "A, B, C" should "sorted" in {
+    assert(isSorted(Array("A", "B", "C"), (a, b) => a < b))
+  }
+
+  "A, B, C, D" should "sorted" in {
+    assert(isSorted(Array("A", "B", "C", "D"), (a, b) => a < b))
+  }
+
+  "A, C, B" should "not be sorted" in {
+    assert(!isSorted(Array("A", "C", "B"), (a, b) => a < b))
+  }
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a, b)
   }
 
 }
